a2d5c90676fdea69857d4fc2d9e4ffa9600b736a
Perla <pataditaenloshuevos@gmail.com>
2019-05-10 13:28:13 -0500

    Updates

---
+++ precache-manifest.52b91d7f157ab5d5865dd9cd68fc435d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605ba735953e2252c7070f3e5e4d97a8",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "e6c3b35fca05c66d0d96",
+    "url": "/GDL002-burger-queen/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "91415285abecff3cbf27",
+    "url": "/GDL002-burger-queen/static/js/2.4fa5e42c.chunk.js"
+  },
+  {
+    "revision": "e6c3b35fca05c66d0d96",
+    "url": "/GDL002-burger-queen/static/js/main.dc127804.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  }
+]);
